5f18284180772784d8652d0c5b4830c8e2b00d9b
Conejosalvaje123 <angelvazquezoriginal@gmail.com>
2024-03-05 16:48:23 -0600

    cards

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,58 @@
 console.log(tempData);
+
+//navbar
+const menuBtn = document.getElementById("menu-btn");
+const menu = document.querySelector(".menu");
+
+menuBtn.addEventListener("click", () => {
+menu.classList.toggle("active");
+});
+
+
+//mouse
+
+const circle = document.querySelector('.circle');
+document.addEventListener('mousemove', (e) => {
+    const x = e.clientX;
+    const y = e.clientY;
+    const circleSize = 925; // Tamaño del círculo
+
+    circle.style.left = `${x - circleSize / 2}px`;
+    circle.style.top = `${y - circleSize / 2}px`;
+});
+
+
+
+
+
+
+
+
+
+// .contenedor-pointing a {
+//     color: #97a1ffb6;
+//     font-size: 30px;
+//     transition: .5s;
+// }
+
+// .contenedor-pointing a::before {
+//     content: '';
+//     top: 0;
+//     left: 0;
+//     width: 100%;
+//     height: 100%;
+//     border-radius: 50%;
+//     background: #100cf1;
+//     transition: .5s;
+//     transform: scale(.9);
+//     z-index: -1;
+// }
+
+// .contenedor-pointing a:hover::before {
+//     transform: scale(1.1);
+//     box-shadow: 0 0 15px #0400fd;
+// }
+
+// .contenedor-pointing a:hover {
+//     color: #0400ff;
+// }
--- front/scripts/tempData.js
+++ front/scripts/tempData.js
@@ -20,13 +20,67 @@ const tempData = [
       "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg",
   },
   {
-    title: "The Lord of the Rings: The Fellowship of the Ring",
-    year: 2001,
-    director: "Peter Jackson",
-    duration: "2h 58min",
-    genre: ["Action", "Adventure", "Drama", "Fantasy"],
+    title: "Your name",
+    year: 2017,
+    director: "Makoto Shinakai",
+    duration: "1h 46min",
+    genre: ["Animation","Drama", "Romance"],
     rate: 8.8,
     poster:
-      "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
+      "https://i.pinimg.com/564x/0a/57/f7/0a57f7fb9bf22d8b357affec4fc3ad03.jpg",
+  },
+  {
+    title: "Your name",
+    year: 2017,
+    director: "Makoto Shinakai",
+    duration: "1h 46min",
+    genre: ["Animation","Drama", "Romance"],
+    rate: 8.8,
+    poster:
+      "https://i.pinimg.com/564x/0a/57/f7/0a57f7fb9bf22d8b357affec4fc3ad03.jpg",
+  },
+  {
+    title: "Your name",
+    year: 2017,
+    director: "Makoto Shinakai",
+    duration: "1h 46min",
+    genre: ["Animation","Drama", "Romance"],
+    rate: 8.8,
+    poster:
+      "https://i.pinimg.com/564x/0a/57/f7/0a57f7fb9bf22d8b357affec4fc3ad03.jpg",
+  },
+  {
+    title: "Your name",
+    year: 2017,
+    director: "Makoto Shinakai",
+    duration: "1h 46min",
+    genre: ["Animation","Drama", "Romance"],
+    rate: 8.8,
+    poster:
+      "https://i.pinimg.com/564x/0a/57/f7/0a57f7fb9bf22d8b357affec4fc3ad03.jpg",
   },
 ];
+
+
+const containerMovie = document.getElementById("flex")
+
+const cardMovie = tempData.map(movie => `
+  
+  <div class="project-card-sm" id = "cards"> 
+  <div class="card-border-gradient"></div>
+  <div class="card-container">
+  <img src = "${movie.poster}">
+  <div class="wrapper">
+  <h2 class = "title">${movie.title}</h2>
+  <p class = "textCard"><strong>Año:</strong> ${movie.year}</p>
+  <p class = "textCard"><strong>Director:</strong> ${movie.director}</p>
+  <p class = "textCard"><strong>Duración:</strong> ${movie.duration}</p>
+  <p class = "textCard"><strong>Genero:</strong> ${movie.genre}</p>
+  <p class = "textCard"><strong>Calificación:</strong> ${movie.rate}</p>
+  </div>
+  </div>
+  </div>
+
+`).join("");
+
+containerMovie.innerHTML = cardMovie;
